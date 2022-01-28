e6ca4bb5285830e5a539007627630d2651124749
catley85 <chriscatley85@gmail.com>
2022-01-28 16:10:24 +0000

    Presentation MVP

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,77 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 // The main funcon is the starting point for all FLutter apps.
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[50],
-        appBar: AppBar(
-          backgroundColor: Colors.pink[800],
-          title: const Text(
-            'I am Rich?',
-          ),
+  runApp(const IAmRich());
+}
+
+class IAmRich extends StatelessWidget {
+  const IAmRich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am Rich?',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(
+        title: 'I am Rich?',
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _randomAsset = 'images/shrug.png';
+  final _assetsArray = [
+    'images/hawk.png',
+    'images/mikey.png',
+    'images/charles.png'
+  ];
+
+  void _selectRandomAsset() {
+    setState(() {
+      final _random = Random();
+      _randomAsset = _assetsArray[_random.nextInt(_assetsArray.length)];
+      debugPrint('ASSET: $_randomAsset');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.title,
         ),
-        body: const Center(
-          child: Image(
-            image: NetworkImage(
-              'https://pbs.twimg.com/profile_images/1181117994488991744/cLZOXTtt_400x400.jpg',
-            ),
+      ),
+      body: Column(children: <Widget>[
+        Image.asset(_randomAsset),
+      ]),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 50.0, left: 16.0, right: 16.0),
+        child: ElevatedButton(
+          onPressed: _selectRandomAsset,
+          child: const Text(
+            'Submit',
+          ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(
+                40), // fromHeight use double.infinity as width and 40 is the height
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
